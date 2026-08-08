e4ad2f1f9786a99f5e44eaf466fe205ada636165
ngullotta <nikolaus.gullotta@protonmail.com>
2026-08-08 15:24:24 -0500

    Starting work on main gb container

--- cmd/gobc/main.go
+++ cmd/gobc/main.go
@@ -3,28 +3,75 @@ package main
 import (
 	"fmt"
 	"gobc/internal/gb"
-	"math"
 	"os"
+	"strings"
 )
 
-func main() {
-	file, err := os.Open(os.Args[1])
+type Cart struct {
+	Title string
+}
 
+type Gameboy struct {
+	cpu  *gb.CPU
+	mmu  *gb.MMU
+	cart *Cart
+	mode gb.Mode
+}
+
+func NewGameboy(romPath string, mode gb.Mode) (*Gameboy, error) {
+	file, err := os.Open(romPath)
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
 	defer file.Close()
 
 	data := make([]byte, 0x8000)
-	file.Read(data)
 
-	cpu := gb.NewCPU()
-	cpu.LoadROM(data)
-
-	fmt.Printf("Loaded Cart: %q\n", cpu.GetCartName())
-
-	cpu.Play()
-	for range int(math.Pow10(7)) {
-		cpu.Step()
+	n, err := file.Read(data)
+	if err != nil || n == 0 {
+		return nil, err
 	}
+
+	title := string(data[0x134:0x142])
+	cart := &Cart{
+		Title: strings.Trim(title, "\x00"),
+	}
+
+	mmu := &gb.MMU{}
+	mmu.Init(mode)
+	mmu.LoadROM(data)
+
+	gb := &Gameboy{
+		cpu:  gb.NewCPU(),
+		mmu:  mmu,
+		cart: cart,
+		mode: mode,
+	}
+
+	return gb, nil
+}
+
+func (gb *Gameboy) GetCart() *Cart { return gb.cart }
+
+func main() {
+	gb, err := NewGameboy(os.Args[1], gb.DMG0)
+	if err != nil {
+		panic(err)
+	}
+
+	if gb == nil {
+		panic("Failed to create gb")
+	}
+
+	cart := gb.GetCart()
+	if cart == nil {
+		panic("WHAT")
+	}
+
+	fmt.Printf("Loaded Cart: %q\n", gb.GetCart().Title)
+
+	// cpu.Play()
+	// for range int(math.Pow10(7)) {
+	// cpu.Step()
+	// }
 }
